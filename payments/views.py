e19167b24d459130ef72b0a4b30a802ae23c5f2a
Tanay Burreja <32588346+tanay1298@users.noreply.github.com>
2024-04-13 03:22:16 +0530

    encrypt qr data

--- payments/views.py
+++ payments/views.py
@@ -1,21 +1,35 @@
+import logging
 from django.conf import settings
-from django.http import JsonResponse
+from django.http import HttpResponseBadRequest, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import qrcode
 import os
+from django.views.decorators.http import require_POST
+import json
+
+_LOGGER = logging.getLogger(__name__)
 
 @csrf_exempt
+@require_POST
 def generate_qr_code(request):
-    if request.method == 'POST':
-        amount = request.POST.get('amount')
-        description = request.POST.get('description')
+    try:
+        data = json.loads(request.body)
+        if not data:
+                return HttpResponseBadRequest('Empty data passed')
         
-        qr = qrcode.make(f"Amount: {amount}, Description: {description}")
+        amount = data.get('amount', None)
+        description = data.get('description', None)
+        if not amount or not description or not data:
+                return HttpResponseBadRequest('amount and description are not passed')
+        
+        payment_details = f"Amount: {amount}, Description: {description}"
+        qr = qrcode.make(payment_details)
         qr_filename = 'qr_code.png'
         qr_path = os.path.join(settings.STATICFILES_DIRS[0], qr_filename)
         qr.save(qr_path)
         qr_url = os.path.join(settings.STATIC_URL, qr_filename)
 
         return JsonResponse({'qr_code_url': qr_url})
-    else:
-        return JsonResponse({'error': 'Only POST requests are allowed'}, status=405)
+    except Exception as error:
+        _LOGGER.error('Error in generate_qr_code API')
+        raise error
